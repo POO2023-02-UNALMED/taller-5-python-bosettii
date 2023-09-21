8a9871cb52eb4e5f67835490ae72f552f6a1c592
bosettii <80700251+bosettii@users.noreply.github.com>
2023-09-20 19:44:26 -0500

    v2.'

--- gestion/zona.py
+++ gestion/zona.py
@@ -1,10 +1,11 @@
 class Zona():
 
-    animales = []
+    #animales = []
 
     def __init__(self,nombre,zoo = None):
         self._nombre=nombre
         self._zoo=zoo
+        self._animales = []
         
     def getNombre(self):
         return self._nombre
@@ -12,10 +13,8 @@ class Zona():
     def getZoo(self):
         return self._zoo
     
-    def agregarAnimales(self,animal,lista = None):
-        if lista == None:
-            lista = []
-        lista.append(animal)
+    def agregarAnimales(self,animal):
+        self._animales.append(animal)
 
     def cantidadAnimales(self):
         return (len(self._animales))
